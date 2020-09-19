a461294233de1c23d81193aa6233a53f3c5d28b1
Apro123 <akapoor3@ucmerced.edu>
2020-09-18 17:52:11 -0700

    modified buildproj script

--- buildproj.sh
+++ buildproj.sh
@@ -1,12 +1,12 @@
 #!/usr/bin/env bash
 case "${TRGT}" in
   browser )
+  ionic integrations enable cordova --quiet
+  ionic cordova platform add browser --no-interactive --confirm
   ionic cordova build browser --prod --release
-  cd www
-  ln -s index.html 404.html
-  ln -s favicon.ico apple-touch-icon.png
-  ln -s favicon.ico apple-touch-icon-precomposed.png
-  cd ..
+  ln -s www/index.html www/404.html
+  ln -s www/favicon.ico www/apple-touch-icon.png
+  ln -s www/favicon.ico www/apple-touch-icon-precomposed.png
   cp platforms/browser/config.xml
   ;;
 esac
